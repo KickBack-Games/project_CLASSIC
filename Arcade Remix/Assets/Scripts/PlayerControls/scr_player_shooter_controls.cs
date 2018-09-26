b13ef133580ad26cc7746119228b6d3f003f35c4
CLEOPATRA\zarof <brandona1990@yahoo.com>
2018-09-25 22:34:05 -0500

    Gradius Game

---
+++ Arcade Remix/Assets/Scripts/PlayerControls/scr_player_shooter_controls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scr_player_shooter_controls : MonoBehaviour {
+    public Button button;
+    public GameObject bullet;
+
+    public int ammo = 5;
+	// Use this for initialization
+	void Start () {
+        button.onClick.AddListener(OnShoot);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnShoot() {
+        if (ammo > 0)
+        {
+            ammo--;
+            GameObject shot = Instantiate(bullet, this.transform.position, bullet.transform.rotation);
+            bullet.SetActive(true);
+        }
+    }
+}
