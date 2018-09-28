2766b9a462f6b7cd851f2d2ee53806db5a7fa331
DESKTOP-O10JD7J\Alec <alec.burmeister@gmail.com>
2018-09-27 19:37:35 -0500

    Thrower game initial mechanics
    Used same sprites from shooter game, lol

---
+++ Arcade Remix/Assets/Scripts/PlayerControls/Thrower/power.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class power : MonoBehaviour {
+
+	// Make a timer
+	private float timer = 10.0f;
+	public int second = 10;
+	public int counter;
+	private bool done = false;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (second > 0)
+		{
+			if (Input.GetMouseButtonDown(0))
+				counter += 1;
+
+			timer -= Time.deltaTime;
+			second = Mathf.RoundToInt(timer);
+		}
+		else
+		{
+			if (!done)
+			{
+				Vector2 test = new Vector2(counter * 2, counter * 2);
+				this.gameObject.GetComponent<Rigidbody2D>().AddForce (test, ForceMode2D.Impulse);
+				done = true;
+			}
+		}
+	}
+}
