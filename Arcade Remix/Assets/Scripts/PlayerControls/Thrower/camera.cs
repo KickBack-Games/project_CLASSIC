9961d8cbcadafc97855343672c0257250b57002f
DESKTOP-O10JD7J\Alec <alec.burmeister@gmail.com>
2018-09-28 20:34:27 -0500

    Added text
    Now see text for time, power, and distance.

---
+++ Arcade Remix/Assets/Scripts/PlayerControls/Thrower/camera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+
+	public GameObject objToFollow;
+	private Vector2 temp = new Vector2();
+	// Update is called once per frame
+	void Update () 
+	{
+		temp = objToFollow.gameObject.GetComponent<Transform>().position;
+		this.gameObject.GetComponent<Transform>().position = new Vector2(temp.x, this.gameObject.GetComponent<Transform>().position.y);
+	}
+}
--- Arcade Remix/Assets/Scripts/PlayerControls/Thrower/power.cs
+++ Arcade Remix/Assets/Scripts/PlayerControls/Thrower/power.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class power : MonoBehaviour {
@@ -7,8 +8,19 @@ public class power : MonoBehaviour {
 	// Make a timer
 	private float timer = 10.0f;
 	public int second = 10;
+
 	public int counter;
 	private bool done = false;
+
+	// Text
+	public Text txtTime;
+	public Text txtDistance; 
+	public float distance;
+
+	void Start ()
+	{
+		setText();
+	}
 
 	// Update is called once per frame
 	void Update () 
@@ -20,15 +32,31 @@ public class power : MonoBehaviour {
 
 			timer -= Time.deltaTime;
 			second = Mathf.RoundToInt(timer);
+			setText();
 		}
 		else
 		{
 			if (!done)
 			{
-				Vector2 test = new Vector2(counter * 2, counter * 2);
-				this.gameObject.GetComponent<Rigidbody2D>().AddForce (test, ForceMode2D.Impulse);
+				Vector2 supahPOWAH = new Vector2(counter * 3, counter * 3);
+				this.gameObject.GetComponent<Rigidbody2D>().AddForce (supahPOWAH, ForceMode2D.Impulse);
+				// Time is redundant now in screen, so change it so that we show total taps instead
+				txtTime.text = counter.ToString();
 				done = true;
 			}
+			else
+			{
+
+				txtDistance.text = (Mathf.FloorToInt(gameObject.GetComponent<Transform>().position.x)).ToString() + " m";
+			}
 		}
+	
+	}
+
+	void setText()
+	{
+		txtDistance.text = counter.ToString();
+		txtTime.text = second.ToString();
+
 	}
 }
