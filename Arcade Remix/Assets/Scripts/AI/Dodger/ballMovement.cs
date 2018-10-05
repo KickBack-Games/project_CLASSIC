ba31cc9113f6d0f8649aa1c437f4c8a1e2f07530
DESKTOP-O10JD7J\Alec <alec.burmeister@gmail.com>
2018-10-04 22:59:12 -0500

    Initial dodger tests
    Trying out a prototype of possible version of it

---
+++ Arcade Remix/Assets/Scripts/AI/Dodger/ballMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballMovement : MonoBehaviour {
+
+
+	public float speed;
+	public int dir;
+
+	void Start()
+	{
+		dir = Random.Range(0, 4);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// Move the object forward along its z axis 1 unit/second.
+		if (dir == 0) // UP
+        	transform.Translate(0, Time.deltaTime * speed, 0);
+        else if(dir == 1) // DOWN
+        	transform.Translate(0, Time.deltaTime * -speed, 0);
+        else if(dir == 2) // RIGHT
+        	transform.Translate(Time.deltaTime * speed, 0, 0);
+        else if(dir == 3) // LEFT
+        	transform.Translate(Time.deltaTime * -speed, 0, 0);
+
+        
+	}
+
+}
+
