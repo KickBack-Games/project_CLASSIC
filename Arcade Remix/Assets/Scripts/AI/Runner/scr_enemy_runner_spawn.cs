e8c6d5ce0ed67c1e11831652b5e312caffc34cf8
CLEOPATRA\zarof <brandona1990@yahoo.com>
2018-10-09 23:01:21 -0500

    Preparing to Run

---
+++ Arcade Remix/Assets/Scripts/AI/Runner/scr_enemy_runner_spawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_enemy_runner_spawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.left * Time.deltaTime * 2);
+    }
+}
