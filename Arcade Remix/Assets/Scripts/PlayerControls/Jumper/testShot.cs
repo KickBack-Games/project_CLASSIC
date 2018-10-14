c7c266ddbe1debba51f085f71ca153d9f0265e53
DESKTOP-O10JD7J\Alec <alec.burmeister@gmail.com>
2018-10-14 00:53:55 -0500

    drag and shoot scriptt is done!
    Main mechanics are functioning as envisioned

--- Arcade Remix/Assets/Scripts/PlayerControls/Jumper/testShot.cs
+++ Arcade Remix/Assets/Scripts/PlayerControls/Jumper/testShot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class testShot : MonoBehaviour {
 
+	public bool locked = false;
 	public bool dragging = false;
 	public bool impulseGiven = false;
 	public float maxVelocity;
@@ -11,22 +12,92 @@ public class testShot : MonoBehaviour {
 	public Rigidbody2D rb;
 	private Vector2 test;
 
+	public Vector2 initP;
+	public Vector2 finalP;
+
+	public float power;
+
 	// Update is called once per frame
 	void Update () 
 	{
+		if (Input.GetMouseButton(0))
+		{
+			// This 'lock' allows the logic for dragging... since only when you let go, does it 
+			// turn false
+			locked = true;
+			// Stop ball from moving as player calculates shot
+			gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+			rb.velocity = new Vector2(0, 0);
+
+			if (locked)
+			{
+				// We can get initial point
+				if ((initP.x == 0) && (initP.y == 0))
+				{
+					initP = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				}
+			} 
+
+		}
+		else
+		{
+			gameObject.GetComponent<Rigidbody2D>().gravityScale = 10;
+			if (locked)
+			{
+				
+
+				// TODO CALCULATIONS with final and init
+				finalP = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+				// Find the direction
+				int xDir;
+				int yDir;
+				if (initP.x > finalP.x)
+					xDir = 1;
+				else
+					xDir = -1;
+
+				if(initP.y > finalP.y)
+					yDir = 1;
+				else
+					yDir = -1;
+
+				// Find the real power (distance between x, and y vectors
+				// subtract the x, and y
+				float xDist = initP.x - finalP.x;
+				float yDist = initP.y - finalP.y;
+
+				xDist = Mathf.Abs(xDist);
+				yDist = Mathf.Abs(yDist);
+				// Can also set a max distance to avoid overpowering
+
+				///  direction (probably 1, or -1... calculated by if statements of initP and finalP)* magnitude * power(which is a public float) 
+				Vector2 supahPOWAH = new Vector2(xDir * power * xDist, yDir * power * yDist);
+				//Vector2 supahPOWAH = new Vector2(initP.x * -1, 30);
+
+				//Do physics
+				gameObject.GetComponent<Rigidbody2D>().AddForce (supahPOWAH, ForceMode2D.Impulse);
+
+				// Reset it
+				initP = new Vector2 (0, 0);
+			}
+			locked = false;
+		} 
 		// if clicked mouse, then drag
 		//GetComponent<Rigidbody2D>().gravityScale = 10;
 		if (dragging)
 		{
 			if (impulseGiven)
 			{
-				Vector2 supahPOWAH = new Vector2(0, 300);
-				gameObject.GetComponent<Rigidbody2D>().AddForce (supahPOWAH, ForceMode2D.Impulse);
-				gameObject.GetComponent<Rigidbody2D>().gravityScale = 5;
-				impulseGiven = false;
+
+				//gameObject.GetComponent<Rigidbody2D>().gravityScale = 5;
+				//impulseGiven = false;
 			}
-			rb.position = test;
+			
+			//rb.position = test;
 		}
+			
 
 	}
 
@@ -41,8 +112,8 @@ public class testShot : MonoBehaviour {
 		test = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		//isPressed = true;
 		//rb.isKinematic = true;
-		dragging = true;
-		gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+		//dragging = true;
+		
 	}
 
 	void OnMouseUp()
@@ -50,8 +121,8 @@ public class testShot : MonoBehaviour {
 	
 		//isPressed = false;
 		//rb.isKinematic = false;
-		dragging = false;
-		impulseGiven = true;
+		//dragging = false;
+		//impulseGiven = true;
 
 	}
 }
