0013a64f6bff434ac61575cfd921d0592de1b86f
CLEOPATRA\zarof <brandona1990@yahoo.com>
2018-10-21 21:25:41 -0500

    Golfing Animation Reset

--- Arcade Remix/Assets/Scripts/AI/golf/scr_golf_ball_aim.cs
+++ Arcade Remix/Assets/Scripts/AI/golf/scr_golf_ball_aim.cs
@@ -6,17 +6,20 @@ public class scr_golf_ball_aim : MonoBehaviour {
     public Vector3 spinSpeed;
     public LineRenderer line;
 
+    public float goSpeed = 0;
+    public GameObject spawner;
     void Start()
     {
         spinSpeed.x = 0;
         spinSpeed.y = 0;
         spinSpeed.z = Random.Range(0.2f, 0.5f);
+        goSpeed = 0;
         line = GetComponent<LineRenderer>();
     }
 
     void Update()
     {
-        Debug.Log(transform.rotation.z);
+        transform.Translate(Quaternion.Euler(0, 0, 0) * this.transform.up * goSpeed);
         gameObject.transform.Rotate(spinSpeed);
         line.SetPosition(1, (Quaternion.Euler(0, 0, 0) * this.transform.up) * 500);
         if (transform.rotation.z > 0.21 && spinSpeed.z > 0)
@@ -26,6 +29,24 @@ public class scr_golf_ball_aim : MonoBehaviour {
         if (transform.rotation.z < -0.21 && spinSpeed.z < 0)
         {
             spinSpeed.z *= -1;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "goal")
+        {
+            Destroy(this.gameObject);
+            spawner.GetComponent<scr_golf_goal_spawn>().OnSpawn();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "MainCamera")
+        {
+            Destroy(this.gameObject);
+            spawner.GetComponent<scr_golf_goal_spawn>().OnSpawn();
         }
     }
 }
---
+++ Arcade Remix/Assets/Scripts/PlayerControls/Golf/scr_player_golf_controls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scr_player_golf_controls : MonoBehaviour {
+    //touch button to fire 
+    public Button button;
+    public GameObject uievent;
+    private Animator anim;
+
+    //hits Limmit
+    public int tries = 5;
+
+    public GameObject ball;
+    // Use this for initialization
+    void Start () {
+        anim = this.GetComponent<Animator>();
+        button.onClick.AddListener(OnShoot);
+        uievent.GetComponent<scr_ui_multiIcon>().OnRefresh(tries);
+        anim.speed = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(anim.GetCurrentAnimatorStateInfo(0).normalizedTime);
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= anim.GetCurrentAnimatorStateInfo(0).length)
+        {
+            if (ball.GetComponent<scr_golf_ball_aim>().goSpeed == 0)
+            {
+                ball.GetComponent<scr_golf_ball_aim>().goSpeed = 0.1f;
+                anim.Play("swing", -1, 0f);
+                anim.speed = 0;
+            }
+        }
+    }
+
+    public void OnShoot() {
+        anim.speed = 1;
+        anim.Play("swing", - 1, 0f);
+        ball.GetComponent<scr_golf_ball_aim>().spinSpeed.z = 0;
+        ball.GetComponent<LineRenderer>().enabled = false;
+    }
+}
