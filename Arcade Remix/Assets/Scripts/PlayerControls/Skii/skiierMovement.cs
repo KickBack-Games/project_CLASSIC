c180c960ce3cbe35a7ff80249c80302df6f6acca
DESKTOP-O10JD7J\Alec <alec.burmeister@gmail.com>
2018-10-23 20:08:29 -0500

    Starting skiing
    Skiing components added

---
+++ Arcade Remix/Assets/Scripts/PlayerControls/Skii/skiierMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skiierMovement : MonoBehaviour {
+
+	public float accel;
+	public Rigidbody2D rb;
+	public float vel;
+	public float yVel;
+
+	private float max;
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+		max = 5;
+		yVel = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Input.GetKey(KeyCode.D)) && (vel < max))
+		{
+			vel += accel;
+		}
+		if (Input.GetKey(KeyCode.A) && (vel > -max))
+		{
+			vel -= accel;
+		}
+		if (transform.position.y > 3f)
+			yVel = -.01f;
+		else
+			yVel = 0f;
+		
+	}
+
+	void FixedUpdate()
+	{
+		rb.velocity =
+		 new Vector2(vel, yVel);
+	}
+}
