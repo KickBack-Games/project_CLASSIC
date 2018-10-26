880fbd4d7f2c48ef8f9e095c2b669040601992dc
CLEOPATRA\zarof <brandona1990@yahoo.com>
2018-10-25 22:55:32 -0500

    Fastball/Slowball

--- Arcade Remix/Assets/Scripts/AI/baseball/scr_baseball_ball.cs
+++ Arcade Remix/Assets/Scripts/AI/baseball/scr_baseball_ball.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class scr_baseball_ball : MonoBehaviour {
     public int vspeed,hspeed;
+    public GameObject pitcher;
     // Use this for initialization
     void Start () {
         vspeed = Random.Range(6, 1) * global.level;
@@ -14,5 +15,14 @@ public class scr_baseball_ball : MonoBehaviour {
 	void Update () {
         transform.Translate(Vector2.down * Time.deltaTime * vspeed);
         transform.Translate(Vector2.right * Time.deltaTime * hspeed);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "MainCamera")
+        {
+            Destroy(this.gameObject);
+            pitcher.GetComponent<scr_baseball_pitcher>().OnShoot();
+        }
     }
 }
---
+++ Arcade Remix/Assets/Scripts/PlayerControls/Baseball/scr_baseball_player_controls.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scr_baseball_player_controls : MonoBehaviour {
+    //touch button to fire 
+    public Button button;
+    public GameObject uievent;
+    private Animator anim;
+
+    //hits Limmit
+    public int tries = 5;
+    // Use this for initialization
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+        button.onClick.AddListener(OnShoot);
+        uievent.GetComponent<scr_ui_multiIcon>().OnRefresh(tries);
+        anim.speed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= anim.GetCurrentAnimatorStateInfo(0).length)
+        {
+            anim.Play("swing", -1, 0f);
+            anim.speed = 0;
+        }
+    }
+
+    public void OnShoot()
+    {
+        anim.speed = 1;
+        anim.Play("swing", -1, 0f);
+    }
+}
