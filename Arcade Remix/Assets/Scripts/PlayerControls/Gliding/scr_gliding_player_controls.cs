2b7a74236fc47df5222bd3af355bc14496d0b5f3
CLEOPATRA\zarof <brandona1990@yahoo.com>
2018-10-28 17:01:42 -0500

    Flappy Hang Glider

---
+++ Arcade Remix/Assets/Scripts/PlayerControls/Gliding/scr_gliding_player_controls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scr_gliding_player_controls : MonoBehaviour {
+    //touch button to fire 
+    public Button button;
+    public GameObject uievent;
+    void Start()
+    {
+        button.onClick.AddListener(OnJump);
+    }
+
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(
+            transform.eulerAngles.x,
+            transform.eulerAngles.y,
+            GetComponent<Rigidbody2D>().velocity.y
+        );
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "spr_player_shooter")
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -3);
+        }
+    }
+
+    public void OnJump()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 5);
+    }
+}
