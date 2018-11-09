2a80a6f680cf7e59de539341e85000aa61e09818
CLEOPATRA\zarof <brandona1990@yahoo.com>
2018-11-08 22:53:58 -0600

    Cruising the Terrain

---
+++ Arcade Remix/Assets/Scripts/PlayerControls/Racer/scr_racer_player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_racer_player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime*3);
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Rotate(new Vector3(0,-1,0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Rotate(new Vector3(0, 1, 0));
+        }
+    }
+}
