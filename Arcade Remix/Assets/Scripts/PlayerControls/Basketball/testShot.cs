a6a919a1e7b251662e49a2f7b95197a54c468a4d
CLEOPATRA\zarof <brandona1990@yahoo.com>
2019-01-01 20:05:24 -0600

    Basketball Woes

--- Arcade Remix/Assets/Scripts/PlayerControls/Basketball/testShot.cs
+++ Arcade Remix/Assets/Scripts/PlayerControls/Basketball/testShot.cs
@@ -2,20 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testShot : MonoBehaviour {
+public class testShot : MonoBehaviour
+{
 
-	public bool locked = false;
-	public float maxVelocity;
+    public bool locked = false;
+    public float maxVelocity;
 
-	public Rigidbody2D rb;
+    public Rigidbody2D rb;
 
-	public Vector2 initP;
-	public Vector2 finalP;
+    public Vector2 initP;
+    public Vector2 finalP;
 
-	public float power;
+    public float power;
 
-	public GameObject anchor;
-	public SpriteRenderer rend;
+    public GameObject anchor;
+    public SpriteRenderer rend;
 
     private float rotSpeed;
 
@@ -23,94 +24,93 @@ public class testShot : MonoBehaviour {
 
     public bool touching;
 
-	void Start()
-	{
+    void Start()
+    {
         points = 0;
         GameObject.Find("EventSystem").GetComponent<scr_ui_multiIcon>().OnRefresh(0);
     }
 
-	void Update ()
-	{
+    void Update()
+    {
         if (Input.GetMouseButton(0) && (gameObject.transform.position.y <= -11))
-		{
-			// This 'lock' allows the logic for dragging... since only when you let go, does it 
-			// turn false
-			locked = true;
-			// Stop ball from moving as player calculates shot
-			gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
-			rb.velocity = new Vector2(0, 0);
+        {
+            // This 'lock' allows the logic for dragging... since only when you let go, does it 
+            // turn false
+            locked = true;
+            // Stop ball from moving as player calculates shot
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+            rb.velocity = new Vector2(0, 0);
 
-			if (locked)
-			{
-				// We can get initial point
-				if ((initP.x == 0) && (initP.y == 0))
-				{
-					initP = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-					rend.transform.position = initP;
-					rend.enabled = true;
-				}
-			} 
-		}
-		else
-		{
-			rend.enabled = false;
-			if (!touching)
-				gameObject.GetComponent<Rigidbody2D>().gravityScale = 10;
+            if (locked)
+            {
+                // We can get initial point
+                if ((initP.x == 0) && (initP.y == 0))
+                {
+                    initP = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    rend.transform.position = initP;
+                    rend.enabled = true;
+                }
+            }
+        }
+        else
+        {
+            rend.enabled = false;
+            if (!touching)
+                gameObject.GetComponent<Rigidbody2D>().gravityScale = 10;
 
-			if (locked)
-			{
-				finalP = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (locked)
+            {
+                finalP = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-				// Find the direction
-				int xDir;
-				int yDir;
-				if (initP.x > finalP.x)
-					xDir = 1;
-				else
-					xDir = -1;
+                // Find the direction
+                int xDir;
+                int yDir;
+                if (initP.x > finalP.x)
+                    xDir = 1;
+                else
+                    xDir = -1;
 
-				if(initP.y > finalP.y)
-					yDir = 1;
-				else
-					yDir = -1;
+                if (initP.y > finalP.y)
+                    yDir = 1;
+                else
+                    yDir = -1;
 
-				// Find the real power (distance between x, and y vectors
-				// subtract the x, and y
-				float xDist = initP.x - finalP.x;
-				float yDist = initP.y - finalP.y;
+                // Find the real power (distance between x, and y vectors
+                // subtract the x, and y
+                float xDist = initP.x - finalP.x;
+                float yDist = initP.y - finalP.y;
 
-				xDist = Mathf.Abs(xDist);
-				yDist = Mathf.Abs(yDist);
-				// Can also set a max distance to avoid overpowering
+                xDist = Mathf.Abs(xDist);
+                yDist = Mathf.Abs(yDist);
+                // Can also set a max distance to avoid overpowering
 
-				///  direction (probably 1, or -1... calculated by if statements of initP and finalP)* magnitude * power(which is a public float) 
-				Vector2 supahPOWAH = new Vector2(xDir * power * xDist, yDir * power * yDist);
+                ///  direction (probably 1, or -1... calculated by if statements of initP and finalP)* magnitude * power(which is a public float) 
+                Vector2 supahPOWAH = new Vector2(xDir * power * xDist, yDir * power * yDist);
 
-				// Do physics
-				gameObject.GetComponent<Rigidbody2D>().AddForce (supahPOWAH, ForceMode2D.Impulse);
+                // Do physics
+                gameObject.GetComponent<Rigidbody2D>().AddForce(supahPOWAH, ForceMode2D.Impulse);
 
-				// Reset it
-				initP = new Vector2 (0, 0);
-				touching = false;
-			}
-			locked = false;
-		} 		
-	}
+                // Reset it
+                initP = new Vector2(0, 0);
+                touching = false;
+            }
+            locked = false;
+        }
+    }
 
-	void FixedUpdate()
-	{
-		rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
-	}
+    void FixedUpdate()
+    {
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
+    }
 
-	void OnTriggerEnter2D(Collider2D other)
-	{
+    void OnTriggerEnter2D(Collider2D other)
+    {
 
-		if(other.gameObject.tag == "stop") // For the opponents hitbox
-		{
-			touching = true;
-			gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
-			rb.velocity = new Vector2(0, 0);
-		}
-	}
+        if (other.gameObject.tag == "stop") // For the opponents hitbox
+        {
+            touching = true;
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+            rb.velocity = new Vector2(0, 0);
+        }
+    }
 }
-
